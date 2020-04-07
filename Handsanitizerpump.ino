68b44fb16a1af6c73847139a7196519e30be971d
H Misfal .K <46556282+hmk1337@users.noreply.github.com>
2020-04-07 23:44:54 +0700

    beta

---
+++ Handsanitizerpump.ino
@@ -0,0 +1,45 @@
+
+#include<Servo.h>
+Servo myservo;
+const int trigPin=3;
+const int echoPin=5; 
+long tmeduration;
+int distance; 
+
+
+
+void setup() {
+
+  myservo.attach(9); // Di PIN 9
+  pinMode(trigPin,OUTPUT);
+  pinMode(echoPin,INPUT);
+  Serial.begin(9600);
+  
+  
+
+}
+
+void loop() {
+  digitalWrite(trigPin,LOW);
+  delayMicroseconds(2);
+  digitalWrite(trigPin,HIGH);
+  delayMicroseconds(10);
+   digitalWrite(trigPin,LOW);
+  
+tmeduration=pulseIn(echoPin,HIGH);
+distance=(0.034*tmeduration)/1;
+
+if(distance<=27){ // Jarak
+  
+  myservo.write(360); //180 atau 360 tergantung servo
+  }
+  else{
+    myservo.write(0);}
+  
+  Serial.print("distance:");
+    Serial.println(distance);
+  
+    delay(1);
+  
+
+}
